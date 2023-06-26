4441b5a0cc4d8f6c9d2bd6b6ebb1d002ecf258f2
kouheisatou <koheisato0420@gmail.com>
2023-06-26 21:26:23 +0900

    FIX : rsync command in run script

--- run_on_server.sh
+++ run_on_server.sh
@@ -3,6 +3,5 @@ if [[ "$#" -ne 5 ]]; then
   exit 0
 fi
 
-sshpass -p "$3" ssh "$2"@"$1" 'rm -rf '"$4"
-sshpass -p "$3" rsync -r -v -e ssh ./ "$2@$1:$4"
+sshpass -p "$3" rsync -r --delete -v -e ssh ./ "$2@$1:$4"
 sshpass -p "$3" ssh "$2"@"$1" 'mkdir -p ~/log/LightningGossipSimulatorOutput/; cd '"$4"'; make build; ./run-simulation.sh 39 ~/log/LightningGossipSimulatorOutput/ '"$5"
