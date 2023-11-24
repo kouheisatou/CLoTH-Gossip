e63e5b4d0cf463833e2fd06846353e38dcee0cc9
kouheisatou <koheisato0420@gmail.com>
2023-11-24 18:48:30 +0900

    FIX : run simulation on separated environment

--- run_on_server.sh
+++ run_on_server.sh
@@ -1,7 +1,12 @@
-if [[ "$#" -ne 5 ]]; then
-  echo "./run_on_server.sh <ip> <user> <pass> <install_dir(full_path)> <output_dir>"
+if [[ "$#" -ne 4 ]]; then
+  echo "./run_on_server.sh <ip> <user> <pass> <output_dir(full_path)>"
   exit 0
 fi
 
-sshpass -p "$3" rsync -r --delete --checksum -v -e ssh ./ "$2@$1:$4"
-sshpass -p "$3" ssh "$2@$1" 'mkdir -p '"$5"'; cd '"$4"'; cmake .; make; GSL_RNG_SEED=39 ./CLoTH_Gossip '"$5"'/; python3 batch-means.py '"$5"'/'
+root_dir="$4/$(date "+%Y%m%d%H%M%S")/"
+output_dir="$root_dir/result/"
+install_dir="$root_dir/environment/"
+
+sshpass -p "$3" ssh "$2@$1" 'mkdir -p '"$output_dir"'; mkdir -p '"$install_dir"''
+sshpass -p "$3" rsync -r --delete --checksum -v -e ssh ./ "$2@$1:$install_dir"
+sshpass -p "$3" ssh "$2@$1" 'mkdir -p '"$output_dir"'; cd '"$install_dir"'; cp cloth_input.txt '"$root_dir"'; cmake .; make; GSL_RNG_SEED=39 ./CLoTH_Gossip '"$output_dir"'/; python3 batch-means.py '"$output_dir"'/'
