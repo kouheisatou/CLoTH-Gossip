603e23ac8db702ec15a0f256a3b0a0683238e85f
kouheisatou <koheisato0420@gmail.com>
2023-12-16 21:53:26 +0900

    FIX : remove background simulation

--- run_all_simulations.sh
+++ run_all_simulations.sh
@@ -58,7 +58,7 @@ change_target="group_limit_rate"
 result_root_dir_1="$output_dir/change_$change_target"
 for ((i = 1; i <= 10; i++)); do
   value=$(printf "%.1f" "$(echo "scale=1; $i / 10" | bc)")
-  run_simulation_background "$seed" "$result_root_dir_1/$change_target=$value" "$change_target=$value" &
+  run_simulation_background "$seed" "$result_root_dir_1/$change_target=$value" "$change_target=$value"
   sleep 5
 done
 
@@ -66,7 +66,7 @@ done
 change_target="group_size"
 result_root_dir_2="$output_dir/change_$change_target"
 for ((i = 1; i <= 10; i++)); do
-  run_simulation_background "$seed" "$result_root_dir_2/$change_target=$i" "$change_target=$i" &
+  run_simulation_background "$seed" "$result_root_dir_2/$change_target=$i" "$change_target=$i"
   sleep 5
 done
 
@@ -78,7 +78,7 @@ for ((i = 0; i <= 10; i++)); do
   if [ "$i" -eq 0 ]; then
       value=100
   fi
-  run_simulation_background "$seed" "$result_root_dir_3/$change_target=$value" "enable_group_routing=false" "$change_target=$value" &
+  run_simulation_background "$seed" "$result_root_dir_3/$change_target=$value" "enable_group_routing=false" "$change_target=$value"
   sleep 5
 done
 
@@ -89,7 +89,7 @@ for ((i = 0; i <= 10; i++)); do
   if [ "$i" -eq 0 ]; then
       value=100
   fi
-  run_simulation_background "$seed" "$result_root_dir_4/$change_target=$value" "enable_group_routing=true" "group_cap_update=true" "$change_target=$value" &
+  run_simulation_background "$seed" "$result_root_dir_4/$change_target=$value" "enable_group_routing=true" "group_cap_update=true" "$change_target=$value"
   sleep 5
 done
 
@@ -100,7 +100,7 @@ for ((i = 0; i <= 10; i++)); do
   if [ "$i" -eq 0 ]; then
       value=100
   fi
-  run_simulation_background "$seed" "$result_root_dir_5/$change_target=$value" "enable_group_routing=true" "group_cap_update=false" "$change_target=$value" &
+  run_simulation_background "$seed" "$result_root_dir_5/$change_target=$value" "enable_group_routing=true" "group_cap_update=false" "$change_target=$value"
   sleep 5
 done
 
