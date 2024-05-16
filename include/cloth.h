b6718fecd310cc0638e76c536e841bd4bc840e2c
kouheisatou <koheisato0420@gmail.com>
2024-05-16 23:00:23 +0900

    FIX : confusion millisatoshi and satoshi

--- include/cloth.h
+++ include/cloth.h
@@ -33,7 +33,6 @@ struct payments_params{
   double inverse_payment_rate;
   long n_payments;
   double average_amount;
-  double variance_amount;
   unsigned int payments_from_file;
   char payments_filename[256];
   unsigned int log_all_events;
