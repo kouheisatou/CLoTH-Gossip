FIX : edge lock history overflow

scripts/analyze_output_and_summarize.py
@@ -187,13 +187,13 @@ def analyze_output(output_dir_name):
                 locked_balance_and_duration = [tuple(map(int, pair.split('x'))) for pair in edge["locked_balance_and_duration"].split("-")]
                 total_locked_balance_and_duration = 0
                 for (locked_balance, locked_duration) in locked_balance_and_duration:
-                    total_locked_balance_and_duration += locked_balance * locked_duration
+                    total_locked_balance_and_duration += locked_balance * locked_duration # [millisatoshi*ms]
                 locked_balance_and_duration_distribution.append(total_locked_balance_and_duration)
 
         result = result | {
             "group_cover_rate": edge_in_group_num / len(edges),  # 全エッジに対するグループに属するエッジが占める割合
 
-            # ロックされた残高×ロックされた時間
+            # edgeごとの、ロックされた残高×ロックされた時間の合計の平均[satoshi*ms]
             "total_locked_balance_duration/average": np.mean(locked_balance_and_duration_distribution),
             "total_locked_balance_duration/variance": np.var(locked_balance_and_duration_distribution),
             "total_locked_balance_duration/max": np.max(locked_balance_and_duration_distribution),