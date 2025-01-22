c092cd7a1c1bf1172b576ef54d30bec7d90a17cb
kouheisatou <koheisato0420@gmail.com>
2025-01-22 23:25:34 +0900

    ADD : simulation for https://github.com/kouheisatou/ieice_okinawa_2025

--- run_all_simulations_broadcastdelay.sh
+++ run_all_simulations_broadcastdelay.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 if [[ "$#" -lt 1 ]]; then
-  echo "./run_all_simulations.sh <seed> <output_dir> <import_from_dijkstra_cache_dir>"
+  echo "./run_all_simulations.sh <output_dir> <import_from_dijkstra_cache_dir>"
   exit 0
 fi
 
-seed="$1"
-
-output_dir="$2/$(date "+%Y%m%d%H%M%S")"
+output_dir="$1/$(date "+%Y%m%d%H%M%S")"
 mkdir "$output_dir"
 
 dijkstra_cache_dir="$output_dir/dijkstra_cache"
 mkdir "$dijkstra_cache_dir"
-if [ "$#" -eq 3 ]; then
-    cp -r "$3/." "$dijkstra_cache_dir/"
+if [ "$#" -eq 2 ]; then
+    cp -r "$2/." "$dijkstra_cache_dir/"
 fi
 
+seed=39
 max_processes=32
 
 queue=()
 running_processes=0
+completed_simulations=0
+completed_simulations_tmp_file="$output_dir/completed_simulations"
 total_simulations=0
 start_time=$(date +%s)
 
@@ -37,6 +38,8 @@ function process_queue() {
 
     wait -n || true
     ((running_processes--))
+    ((completed_simulations++))
+    echo "$completed_simulations" > "$completed_simulations_tmp_file"
 }
 
 function display_progress() {
@@ -44,57 +47,49 @@ function display_progress() {
         return 0
     fi
 
-    done_simulations=0
+    last_updated=0
+    last_estimated_time=0
+    count=0
 
-    while [ "$done_simulations" -lt "$total_simulations" ]; do
-        progress_summary=""
-        total_progress=0
+    completed_simulations_from_tmp_file=0
+    echo "$completed_simulations_from_tmp_file" > "$completed_simulations_tmp_file"
 
-        # read each simulation progresses
-        IFS=$'\n' read -r -d '' -a simulation_progress_files <<< $(find "$output_dir" -type f -name "progress.tmp")
-        done_simulations=0
-        for file in "${simulation_progress_files[@]}"; do
-            progress=$(cat "$file")
-            if [ "$progress" = "1" ]; then
-              done_simulations=$((done_simulations + 1))
-            elif [ "$progress" = "" ]; then
-              progress="0"
-            fi
-            total_progress=$(printf "%.5f" "$(echo "scale=4; $total_progress + $progress / $total_simulations" | bc)")
-            progress_summary="$progress_summary$(printf "%3d%% %s" "$(printf "%.0f" "$(echo "$progress*100" | bc)")" "$file")\n"
-        done
+    while [ "$completed_simulations_from_tmp_file" -lt "$total_simulations" ]; do
 
-        # build progress bar
-        progress_bar_len=$(printf "%0.s#" $(seq 1 $(printf "%.0f" "$(echo "$total_progress * 100 / 2" | bc)")))
-        progress_bar=""
-        if [ $(python3 -c "print($total_progress==0)") = "True" ]; then
-            progress_bar=$(printf "Progress: [%-50s] 0%%\t%d/%d\t Time remaining --:--" "" "$done_simulations" "$total_simulations")
-        else
-            elapsed_time=$(( $(date +%s) - start_time ))
-            estimated_completion_time=$(python3 -c "print(int($elapsed_time / $total_progress - $elapsed_time))")
-            remaining_minutes=$(( estimated_completion_time / 60 ))
-            remaining_seconds=$(( estimated_completion_time % 60 ))
-            progress_bar=$(printf "Progress: [%-50s] %0.1f%%\t%d/%d\t Time remaining %02d:%02d" "$progress_bar_len" "$(echo "scale=1; $total_progress * 100" | bc)" "$done_simulations" "$total_simulations" "$remaining_minutes" "$remaining_seconds")
+        text=$(cat "$completed_simulations_tmp_file")
+        if [ -n "$text" ]; then
+            completed_simulations_from_tmp_file="$text"
         fi
 
-        echo -e "$progress_summary$progress_bar"
+        progress=$(python3 -c "print($completed_simulations_from_tmp_file / $total_simulations)")
+        progress_bar=$(printf "%0.s#" $(seq 1 $((completed_simulations_from_tmp_file * 100 / total_simulations / 2))))
+
+        if [ "$completed_simulations_from_tmp_file" -eq 0 ]; then
+            printf "\rProgress: [%-50s] 0%%\t%d/%d\t Time remaining --:--\t" "" "$completed_simulations_from_tmp_file" "$total_simulations"
+        else
+            elapsed_time=$(( $(date +%s) - start_time ))
+            estimated_completion_time=$(python3 -c "print(int($elapsed_time / $progress - $elapsed_time))")
+            if [ "$completed_simulations_from_tmp_file" -eq "$last_updated" ]; then
+                estimated_completion_time=$((last_estimated_time - count))
+                count=$((count + 1))
+            else
+                last_updated="$completed_simulations_from_tmp_file"
+                last_estimated_time="$estimated_completion_time"
+                count=0
+            fi
+            remaining_minutes=$(( estimated_completion_time / 60 ))
+            remaining_seconds=$(( estimated_completion_time % 60 ))
+            printf "\rProgress: [%-50s] %0.1f%%\t%d/%d\t Time remaining %02d:%02d\t" "$progress_bar" "$(echo "scale=1; $progress * 100" | bc)" "$completed_simulations_from_tmp_file" "$total_simulations" "$remaining_minutes" "$remaining_seconds"
+        fi
         sleep 1
     done
 }
 
-n_payments="50000"  # based on simulation settings used by CLoTH paper https://www.sciencedirect.com/science/article/pii/S2352711021000613
-for j in $(seq 0 100 5000); do
-    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=channel_update/avg_pmt_amt=1000/group_broadcast_delay=$j       $dijkstra_cache_dir/method=channel_update,seed=$seed,n_payments=$n_payments,avg_pmt_amt=1000    payment_rate=10000 n_payments=$n_payments mpp=0 routing_method=channel_update     group_cap_update=true     variance_payment_forward_interval=100   average_payment_amount=1000   group_size=5 group_limit_rate=0.1 group_broadcast_delay=$j"
-    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=ideal/avg_pmt_amt=1000/group_broadcast_delay=$j                $dijkstra_cache_dir/method=ideal,seed=$seed,n_payments=$n_payments,avg_pmt_amt=1000             payment_rate=10000 n_payments=$n_payments mpp=0 routing_method=ideal              group_cap_update=true     variance_payment_forward_interval=100   average_payment_amount=1000   group_size=5 group_limit_rate=0.1 group_broadcast_delay=$j"
-    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=group_routing/avg_pmt_amt=1000/group_broadcast_delay=$j        $dijkstra_cache_dir/method=group_routing,seed=$seed,n_payments=$n_payments,avg_pmt_amt=1000     payment_rate=10000 n_payments=$n_payments mpp=0 routing_method=group_routing      group_cap_update=true     variance_payment_forward_interval=100   average_payment_amount=1000   group_size=5 group_limit_rate=0.1 group_broadcast_delay=$j"
-
-    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=channel_update/avg_pmt_amt=10000/group_broadcast_delay=$j      $dijkstra_cache_dir/method=channel_update,seed=$seed,n_payments=$n_payments,avg_pmt_amt=10000   payment_rate=10000 n_payments=$n_payments mpp=0 routing_method=channel_update     group_cap_update=true     variance_payment_forward_interval=100   average_payment_amount=10000  group_size=5 group_limit_rate=0.1 group_broadcast_delay=$j"
-    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=ideal/avg_pmt_amt=10000/group_broadcast_delay=$j               $dijkstra_cache_dir/method=ideal,seed=$seed,n_payments=$n_payments,avg_pmt_amt=10000            payment_rate=10000 n_payments=$n_payments mpp=0 routing_method=ideal              group_cap_update=true     variance_payment_forward_interval=100   average_payment_amount=10000  group_size=5 group_limit_rate=0.1 group_broadcast_delay=$j"
-    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=group_routing/avg_pmt_amt=10000/group_broadcast_delay=$j       $dijkstra_cache_dir/method=group_routing,seed=$seed,n_payments=$n_payments,avg_pmt_amt=10000    payment_rate=10000 n_payments=$n_payments mpp=0 routing_method=group_routing      group_cap_update=true     variance_payment_forward_interval=100   average_payment_amount=10000  group_size=5 group_limit_rate=0.1 group_broadcast_delay=$j"
-
-    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=channel_update/avg_pmt_amt=100000/group_broadcast_delay=$j     $dijkstra_cache_dir/method=channel_update,seed=$seed,n_payments=$n_payments,avg_pmt_amt=100000  payment_rate=10000 n_payments=$n_payments mpp=0 routing_method=channel_update     group_cap_update=true     variance_payment_forward_interval=100   average_payment_amount=100000 group_size=5 group_limit_rate=0.1 group_broadcast_delay=$j"
-    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=ideal/avg_pmt_amt=100000/group_broadcast_delay=$j              $dijkstra_cache_dir/method=ideal,seed=$seed,n_payments=$n_payments,avg_pmt_amt=100000           payment_rate=10000 n_payments=$n_payments mpp=0 routing_method=ideal              group_cap_update=true     variance_payment_forward_interval=100   average_payment_amount=100000 group_size=5 group_limit_rate=0.1 group_broadcast_delay=$j"
-    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=group_routing/avg_pmt_amt=100000/group_broadcast_delay=$j      $dijkstra_cache_dir/method=group_routing,seed=$seed,n_payments=$n_payments,avg_pmt_amt=100000   payment_rate=10000 n_payments=$n_payments mpp=0 routing_method=group_routing      group_cap_update=true     variance_payment_forward_interval=100   average_payment_amount=100000 group_size=5 group_limit_rate=0.1 group_broadcast_delay=$j"
+for i in $(seq 0 100 5000); do
+    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=group_routing/average_payment_amount=100/broadcast_delay=$i     $dijkstra_cache_dir/method=group_routing,avg_pmt_amt=100     group_broadcast_delay=$i n_payments=50000 mpp=0 routing_method=group_routing   average_payment_amount=100     "
+    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=group_routing/average_payment_amount=1000/broadcast_delay=$i    $dijkstra_cache_dir/method=group_routing,avg_pmt_amt=1000    group_broadcast_delay=$i n_payments=50000 mpp=0 routing_method=group_routing   average_payment_amount=1000    "
+    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=group_routing/average_payment_amount=10000/broadcast_delay=$i   $dijkstra_cache_dir/method=group_routing,avg_pmt_amt=10000   group_broadcast_delay=$i n_payments=50000 mpp=0 routing_method=group_routing   average_payment_amount=10000   "
+    enqueue_simulation "./run-simulation.sh $seed $output_dir/routing_method=group_routing/average_payment_amount=100000/broadcast_delay=$i  $dijkstra_cache_dir/method=group_routing,avg_pmt_amt=100000  group_broadcast_delay=$i n_payments=50000 mpp=0 routing_method=group_routing   average_payment_amount=100000  "
 done
 
 # Process the queue
@@ -104,9 +99,7 @@ while [ "${#queue[@]}" -gt 0 ] || [ "$running_processes" -gt 0 ]; do
     sleep 1
 done
 wait
-echo -e "\nAll simulations have completed. \nOutputs saved at $output_dir"
+echo -e "\nAll simulations have completed."
 python3 scripts/analyze_output_and_summarize.py "$output_dir"
-end_time=$(date +%s)
 echo "START : $(date --date @"$start_time")"
-echo "  END : $(date --date @"$end_time")"
-echo " TIME : $((end_time - start_time))"
+echo "  END : $(date)"
