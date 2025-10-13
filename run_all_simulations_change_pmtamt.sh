6bcaf1b80550aa442abf02751302c611299de06a
kouheisatou <koheisato0420@gmail.com>
2025-10-13 17:00:33 +0900

    FIX : simulation script alpha beta

--- run_all_simulations_change_pmtamt.sh
+++ run_all_simulations_change_pmtamt.sh
@@ -82,8 +82,10 @@ for i in $(seq 1.0 0.2 5.0); do
     enqueue_simulation         "./run-simulation.sh $seed $output_dir/routing_method=cloth_original/average_payment_amount=$avg_pmt_amt                                   payment_timeout=-1 n_payments=5000 mpp=0 routing_method=cloth_original      average_payment_amount=$avg_pmt_amt  variance_payment_amount=$var_pmt_amt  group_size=  "
     enqueue_simulation         "./run-simulation.sh $seed $output_dir/routing_method=ideal/average_payment_amount=$avg_pmt_amt                                            payment_timeout=-1 n_payments=5000 mpp=0 routing_method=ideal               average_payment_amount=$avg_pmt_amt  variance_payment_amount=$var_pmt_amt  group_size=  "
     enqueue_simulation         "./run-simulation.sh $seed $output_dir/routing_method=group_routing/average_payment_amount=$avg_pmt_amt                                    payment_timeout=-1 n_payments=5000 mpp=0 routing_method=group_routing       average_payment_amount=$avg_pmt_amt  variance_payment_amount=$var_pmt_amt  group_size=10"
-    for cul_threshold_dist_beta in $(seq 1 2 20); do
-        enqueue_simulation         "./run-simulation.sh $seed $output_dir/routing_method=group_routing_cul/cul_threshold_dist_beta=$cul_threshold_dist_beta/average_payment_amount=$avg_pmt_amt                                payment_timeout=-1 n_payments=5000 mpp=0 routing_method=group_routing_cul   average_payment_amount=$avg_pmt_amt  variance_payment_amount=$var_pmt_amt  group_size=10 cul_threshold_dist_alpha=2 cul_threshold_dist_beta=$cul_threshold_dist_beta"
+    for cul_threshold_dist_alpha in $(seq 1 2 20); do
+        for cul_threshold_dist_beta in $(seq 1 2 20); do
+            enqueue_simulation         "./run-simulation.sh $seed $output_dir/routing_method=group_routing_cul/cul_threshold_dist_alpha=$cul_threshold_dist_alpha/cul_threshold_dist_beta=$cul_threshold_dist_beta/average_payment_amount=$avg_pmt_amt                                payment_timeout=-1 n_payments=5000 mpp=0 routing_method=group_routing_cul   average_payment_amount=$avg_pmt_amt  variance_payment_amount=$var_pmt_amt  group_size=10 cul_threshold_dist_alpha=$cul_threshold_dist_alpha cul_threshold_dist_beta=$cul_threshold_dist_beta"
+        done
     done
 done
 
